c7929ada412dc74f474417fdadf363d167ec7ca5
Ernesto Dalva de Medeiros <ernesto.medeiros@great.local.ufc.br>
2025-11-09 14:38:29 -0300

    .

--- backend/src/main/java/com/example/demo/security/configs/SecurityConfig.java
+++ backend/src/main/java/com/example/demo/security/configs/SecurityConfig.java
@@ -42,6 +42,7 @@
                 // Set permissions on endpoints
                 .authorizeHttpRequests( auth -> auth
                 // public endpoints
+                                .requestMatchers(HttpMethod.GET,"/candies/**").permitAll()
                     .requestMatchers(HttpMethod.POST, "api/auth/signup/**").permitAll()
                     .requestMatchers(HttpMethod.POST, "api/auth/login/**").permitAll()
                     .requestMatchers(HttpMethod.GET, "api-docs/**").permitAll()
