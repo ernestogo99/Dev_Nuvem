12dff0fb24b8bdd882c1b995b413c1c084849f5a
Guilherme Barbosa <guilhermecirineu@hotmail.com>
2025-12-24 10:49:14 -0300

    fix: adjust MinIO configuration

--- backend/src/main/java/com/example/demo/infra/aws/AWSConfig.java
+++ backend/src/main/java/com/example/demo/infra/aws/AWSConfig.java
@@ -9,9 +9,7 @@ import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.presigner.S3Presigner;
-import software.amazon.awssdk.services.sqs.SqsClient;
+
 
 @Configuration
 public class AWSConfig {
@@ -45,47 +43,5 @@ public class AWSConfig {
                 .dynamoDbClient(dynamoDbClient)
                 .build();
     }
-
-    @Bean
-    public S3Client s3Client() {
-
-        AwsSessionCredentials sessionCredentials = AwsSessionCredentials.create(
-                awsAccessKey, 
-                awsSecretAccess, 
-                sessionToken
-        );
-
-        return S3Client.builder()
-                .region(Region.of(awsRegion))
-                .credentialsProvider(StaticCredentialsProvider.create(sessionCredentials))
-                .build();
-    }
-
-    @Bean
-    public S3Presigner s3Presigner() {
-
-        AwsSessionCredentials sessionCredentials = AwsSessionCredentials.create(
-                awsAccessKey, 
-                awsSecretAccess, 
-                sessionToken
-        );
-
-        return S3Presigner.builder()
-                .region(Region.of(awsRegion))
-                .credentialsProvider(StaticCredentialsProvider.create(sessionCredentials))
-                .build();
-    }
-
-    @Bean
-    public SqsClient sqsClient(){
-        var awsCredentials =  AwsSessionCredentials.create(awsAccessKey, awsSecretAccess, sessionToken);
-        AwsCredentialsProvider credentialsProvider = StaticCredentialsProvider.create(awsCredentials);
-
-        return SqsClient.builder()
-                .region((Region.of(awsRegion)))
-                .credentialsProvider(credentialsProvider)
-                .build();
-    }
-
 
 }
